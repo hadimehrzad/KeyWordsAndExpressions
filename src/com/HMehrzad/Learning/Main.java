63d4fa07b6800c00683df837ee13feb8f8ac9ac9
hadimehrzad <61123461+hadimehrzad@users.noreply.github.com>
2020-02-27 21:55:07 +0330

    Ex8: KeyWords And Expressions

---
+++ src/com/HMehrzad/Learning/Main.java
@@ -0,0 +1,21 @@
+package com.HMehrzad.Learning;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        // a mile is equal to 1.609344 kilometres
+        double kilometres = (100 * 1.609344);
+        int highScore = 50;
+        if (highScore == 50) {
+            System.out.println("This is an expression");
+        }
+        /* In the following code that i will type below, write down what parts of the code
+        are expressions.*/
+        int score = 100;
+        if (score > 90) {
+            System.out.println("You got the high score");
+            score = 0;
+        }
+    }
+}
